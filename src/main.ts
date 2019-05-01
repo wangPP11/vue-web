1896ca0d319c979f6c715889da6fe72cda7ad688
bertram.wang <18170146395@163.com>
2019-05-01 09:22:22 +0800

    修改文件路径

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import router from './config/router';
+import store from './config/store';
+import './config/registerServiceWorker';
 import axios from "./config/axios.tool";
 import vueAxios from  "vue-axios";
 import ElementUI from 'element-ui';
